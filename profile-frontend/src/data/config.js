1eedb29bdbb998bd98759208b217aed9c49ac55e
arijit sahoo <tijira1999sahoo@gmail.com>
2021-01-19 22:45:04 +0530

    final build

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Arijit Sahoo",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Arijit Sahoo and I'm a Software Engineer!",
+  githubUrl: "https://github.com/tijira-1999",
+  linkedinUrl: "https://www.linkedin.com/in/arijit-sahoo-62b099183/",
+  cvLink: "https://docs.google.com/document/d/1y7P5wbuGW2PU1f9kxp4OYnRVD_XPXEEXanelU935bU8/edit?usp=sharing",
+};
